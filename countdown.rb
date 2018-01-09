129bab8a38104e510d91ffaa2aa2929116a6b15d
Ryan Farney <ryanfarney@Ryans-MacBook-Pro-104.local>
2018-01-08 16:36:15 -0800

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(n)
+	while n
+		puts "#{n} SECOND(S)!"
+		n -= 1
+		break if n == 0
+	end
+	return 'HAPPY NEW YEAR!'
+end
+
+def countdown_with_sleep(n)
+	while n
+		puts "#{n} SECOND(S)!"
+		n -= 1
+		sleep 1
+		break if n == 0
+	end
+	return 'HAPPY NEW YEAR!'
 end
